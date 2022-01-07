bffbef9528602cb745aec6f202d838ebb8fb170a
9ary <streetwalkermc@gmail.com>
2022-01-08 00:45:12 +0800

    sm8150-common: implement ALS compensation algorithm
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,6 +58,9 @@ TARGET_PRODUCT_PROP += $(VENDOR_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(VENDOR_PATH)/system.prop
 TARGET_VENDOR_PROP += $(VENDOR_PATH)/vendor.prop
 
+# Treble
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+
 # A/B
 AB_OTA_UPDATER := true
 
