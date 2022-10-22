ca54d8182d56953e3006ca15a173b8caf68b4736
AnierinB <anierin@evolution-x.org>
2022-10-22 01:05:21 +0000

    sm8150-common: Configure & ship OPlusExtras
    - Device specific features are set in their respective trees.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Change-Id: I5344ac177e49e850fa772f818535b144c9db6308

--- common.mk
+++ common.mk
@@ -17,9 +17,9 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     otapreopt_script
 
-# Alert slider
+# OPlusExtras
 PRODUCT_PACKAGES += \
-    KeyHandler \
+    OPlusExtras \
     tri-state-key-calibrate
 
 # Audio
@@ -183,6 +183,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.oplus.rc \
+    init.oplus_extras.rc \
     init.oplus.sh \
     init.qcom.early_boot.sh \
     init.qcom.rc \
@@ -247,7 +248,8 @@ PRODUCT_PACKAGES += \
     NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
-    OPlusSystemUIResCommon
+    OPlusSystemUIResCommon \
+    OPlusExtrasResCommon
 
 # Partitions
 PRODUCT_PACKAGES += \
