44721c1210b808990606d96270bcefd11f877181
John Galt <johngaltfirstrun@gmail.com>
2023-07-01 08:50:34 -0400

    sm8150-common: Drop perf
     Poor on drains, use minimal in kernel solutions for now
    
    Change-Id: I3dd7275fa7997cd85341a23ea1b973e734eaac99

--- common.mk
+++ common.mk
@@ -347,9 +347,7 @@ PRODUCT_BOOT_JARS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power-service-qti
 
 # QMI
 PRODUCT_PACKAGES += \
