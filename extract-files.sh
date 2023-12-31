a6427abe6608869719b32dd0e0874226f3c78ef3
Pranav Vashi <neobuddy89@gmail.com>
2024-01-01 01:09:09 +0530

    sm8150-common: Link dolby blobs with v33 libstagefright_foundation
    Change-Id: Ic5084220aa15f00630dcd4030e7dc37c67486bc3

--- extract-files.sh
+++ extract-files.sh
@@ -79,6 +79,11 @@ function blob_fixup() {
         vendor/lib64/hw/com.qti.chi.override.so)
             grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
             ;;
+        vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefright_soft_ac4dec.so | \
+        vendor/lib/libstagefrightdolby.so | vendor/lib64/libstagefright_soft_ddpdec.so | \
+        vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefrightdolby.so)
+            ${PATCHELF} --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
