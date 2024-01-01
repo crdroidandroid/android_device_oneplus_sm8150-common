f973a4bcdfecc13c2017eb9ece030ceb6be71649
ShevT <evgeny.shishkov@gmail.com>
2024-01-01 09:13:31 +0300

    sm8150-common: Revert OnePlus Dolby
    Revert "sm8150-common: Update dolby config"
    
    This reverts commit 1c1aa6a8cfe5a5c3d8ac968bf2a9d4977a693b7d.
    
    Revert "sm8150-common: Link libdlbdsservice and libhwdap against v33 libstagefright_foundation"
    
    This reverts commit c80a95a0241f175d7a99a9219f00e1f1cfd4e2f5.
    
    Revert "sm8150-common: Introduce Dolby Atmos support"
    
    This reverts commit 8883092712dcd1c1f70ebb78644d422f41424d76.
    
    Change-Id: I7f45ce2fefbc8a9925468590bf9c40998f5e6733

--- extract-files.sh
+++ extract-files.sh
@@ -79,9 +79,6 @@ function blob_fixup() {
         vendor/lib64/hw/com.qti.chi.override.so)
             grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
             ;;
-        odm/lib/libdlbdsservice_v3_6.so | odm/lib/libstagefright_soft_ddpdec.so | odm/lib/libstagefrightdolby.so | odm/lib64/libdlbdsservice_v3_6.so)
-            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
-            ;;
     esac
 }
 
